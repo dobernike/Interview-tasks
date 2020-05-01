c05df7b8159034196ba27361d7ef1556c52d9b22
dobernike <Dobernike@mail.ru>
2020-05-01 16:08:35 +0300

    Added solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// #1
+function steps1(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(stair);
+    }
+}
+
+// #2
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    const add = stair.length <= row ? '#' : ' ';
+    steps(n, row, stair + add);
+}
 
 module.exports = steps;
