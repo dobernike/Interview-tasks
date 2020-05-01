45316cb6dcbbde0ec863d67b000af5b2ecf23c2b
dobernike <Dobernike@mail.ru>
2020-05-01 20:55:52 +0300

    Added solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// #1
+function vowels1(str) {
+    let count = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// #2
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+// #m
+function vowelsM(str) {
+    const vows = ['a', 'e', 'i', 'o', 'u'];
+
+    return str
+        .toLowerCase()
+        .split('')
+        .filter((char) => vows.includes(char)).length;
+}
 
 module.exports = vowels;
