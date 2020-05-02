296ce1441cb0e6806b8d0353ffbd74a180ef324d
dobernike <Dobernike@mail.ru>
2020-05-02 13:57:38 +0300

    memoization solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -36,6 +36,31 @@ function fib2(n) {
 for improve this, need use memoization
 */
 
+// #3 memoize recursive
+function memoize(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
 // #m
 function fibM(n) {
     let curr = 0;
