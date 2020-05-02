813d22062c1f67d7484eb6bdc7f4ed1c777d06ba
dobernike <Dobernike@mail.ru>
2020-05-02 13:34:06 +0300

    Added solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,46 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// #1 loop Linear O(n)
+function fib1(n) {
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        // const a = result[result.length - 1];
+        const a = result[i - 1];
+        const b = result[i - 2];
+
+        result.push(a + b);
+    }
+
+    return result[n];
+}
+
+// #2 recursive Exponential O(2^n)
+function fib2(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+/*
+for improve this, need use memoization
+*/
+
+// #m
+function fibM(n) {
+    let curr = 0;
+    let next = 1;
+
+    for (let i = 0; i < n; i++) {
+        const tmp = next;
+        next = next + curr;
+        curr = tmp;
+    }
+
+    return curr;
+}
 
 module.exports = fib;
