1d4d3bb7df65cc86cca03de61c6c2b94759f468b
dobernike <Dobernike@mail.ru>
2020-05-04 09:29:03 +0300

    Added selectionSort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,25 @@ function bubbleSort(arr) {
     return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let indexOfMin = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[indexOfMin]) {
+                indexOfMin = j;
+            }
+        }
+
+        if (indexOfMin !== i) {
+            const lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
+
+    return arr;
+}
 
 function mergeSort(arr) {}
 
