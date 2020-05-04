b843827415a7a16562723fa3646a94d1295da1f1
dobernike <Dobernike@mail.ru>
2020-05-04 11:21:34 +0300

    Added mergeSort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,12 +32,33 @@ function selectionSort(arr) {
             arr[i] = lesser;
         }
     }
-
     return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
-function merge(left, right) {}
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    const result = [];
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    return [...result, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
